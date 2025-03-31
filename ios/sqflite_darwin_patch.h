086e0f842916d152f0ff9f3aa2eec9cdb8f6a490
Sylvester Dsouza <sylvester310dsouza@gmail.com>
2025-03-31 23:57:12 +0530

    update 100

--- ios/sqflite_darwin_patch.h
+++
@@ -1,9 +0,0 @@
-// sqflite_darwin_patch.h
-// This header defines necessary SQLite flags for sqflite_darwin
-
-#ifndef SQFLITE_DARWIN_PATCH_H
-#define SQFLITE_DARWIN_PATCH_H
-
-#define SQLITE_ENABLE_COLUMN_METADATA 1
-
-#endif /* SQFLITE_DARWIN_PATCH_H */
--- macos/Flutter/GeneratedPluginRegistrant.swift
+++ macos/Flutter/GeneratedPluginRegistrant.swift
@@ -11,7 +11,6 @@ import firebase_core
 import firebase_messaging
 import flutter_secure_storage_macos
 import google_sign_in_ios
-import open_file_mac
 import path_provider_foundation
 import shared_preferences_foundation
 import sqflite_darwin
@@ -25,7 +24,6 @@ func RegisterGeneratedPlugins(registry: FlutterPluginRegistry) {
   FLTFirebaseMessagingPlugin.register(with: registry.registrar(forPlugin: "FLTFirebaseMessagingPlugin"))
   FlutterSecureStoragePlugin.register(with: registry.registrar(forPlugin: "FlutterSecureStoragePlugin"))
   FLTGoogleSignInPlugin.register(with: registry.registrar(forPlugin: "FLTGoogleSignInPlugin"))
-  OpenFilePlugin.register(with: registry.registrar(forPlugin: "OpenFilePlugin"))
   PathProviderPlugin.register(with: registry.registrar(forPlugin: "PathProviderPlugin"))
   SharedPreferencesPlugin.register(with: registry.registrar(forPlugin: "SharedPreferencesPlugin"))
   SqflitePlugin.register(with: registry.registrar(forPlugin: "SqflitePlugin"))
